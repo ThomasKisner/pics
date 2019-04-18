e332fdced4e9e33bd779bdbd94c10880169d6e54
thomaskisner <tjkisner@gmail.com>
2019-04-18 10:49:05 -0400

    created an image card which uses refs to dynamically allocate grid space for the images of varying sizes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Component } from "react";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 import unsplash from "../api/unsplash";
 
 class App extends Component {
   state = {
-    images: {}
+    images: []
   };
 
   onSearchSubmit = async term => {
@@ -21,7 +22,7 @@ class App extends Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length}
+        <ImageList images={this.state.images}/>
       </div>
     );
   }
